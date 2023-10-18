6deee5d8332ee4f84d5d219f5b7f18047c154f2c
GabrielDoms <gabrieldoms@hotmail.com>
2023-10-18 18:11:55 -0300

    Versão Final

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import {} from 'react'
+import { FaFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6"; 
+import { FaLinkedin } from "react-icons/fa6"; 
+import { FaVoicemail } from "react-icons/fa6";
+
+
+function Ropade (){
+  return(
+    <section >
+    <FaFacebook />
+    <FaInstagram />
+    <FaLinkedin/>
+    <FaVoicemail/>
+    <h3 className='fotter-letra'>@2023 - Todos os direitos reservados - La Fiesta Saborosa</h3>
+    </section>
+  )
+}
+export default Ropade
---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,5 @@
+export const ListaProdutos =[
+  {id: 1, nome: "Guacamole", desc: "Seria um abacate amassado com tortilhas.", valor: 45},
+  {id: 2, nome: "Tacos", desc: "Tacos são pastilhas com deliciosos recheios.", valor: 25},
+  {id: 3, nome: "Quesadilha", desc: "Uma deliciosa torta com toques de pimentas", valor: 70},
+]
